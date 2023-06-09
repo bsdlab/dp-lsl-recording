fbd31b4f884276c5bb5db60c6bb61c0e91c58f27
matthiasdold <matthiasdold@gmx.net>
2023-06-09 09:50:38 +0200

    updated and blacked

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # LSL Recording
 
-This a convenient wrapper, bridging to control the LSL recorder via a Dareplane API server.
+This a convenient wrapper, bridging to control the [LSL](https://labstreaminglayer.org/#/) [LabRecorder](https://github.com/labstreaminglayer/App-LabRecorder) via a Dareplane API server.
+
 If you use this module, right now you manually need to start the [`LSL LabRecorder`](https://github.com/labstreaminglayer/App-LabRecorder) before spawning the module. This is a limitation of the current implementation.
 
-Starting up the LabRecorder will be automated in the future.
+Starting up the [LabRecorder](https://github.com/labstreaminglayer/App-LabRecorder) will be automated in the future.
--- lsl_recorder/controller.py
+++ lsl_recorder/controller.py
@@ -16,10 +16,10 @@ from lsl_recorder.utils.logging import logger
 
 class LSLRecorderCom(object):
 
-    """ Communication object to use TCP API of LSL APP-LabRecorder"""
+    """Communication object to use TCP API of LSL APP-LabRecorder"""
 
-    def __init__(self, data_root: Path =Path('.'), addr="localhost", port=22345):
-        """ Create the object with an appropriate socket
+    def __init__(self, data_root: Path = Path("."), addr="localhost", port=22345):
+        """Create the object with an appropriate socket
 
         Parameters
         ----------
@@ -43,11 +43,13 @@ class LSLRecorderCom(object):
         self._socket.sendall(b"select all\n")
         return 0
 
-    def set_recording_file(self, fname: str, data_root: str | Path | None = None, overwrite: bool = False):
+    def set_recording_file(
+        self, fname: str, data_root: str | Path | None = None, overwrite: bool = False
+    ):
         self.update()
         time.sleep(2)
 
-        data_root = Path(data_root)   # make sure it is a Path
+        data_root = Path(data_root)  # make sure it is a Path
 
         if data_root is not None:
             logger.debug(f"Overwriting data root for lsl recorder to: {data_root}")
@@ -55,14 +57,15 @@ class LSLRecorderCom(object):
 
         # Increment with time if already exists
 
-        fname = self._data_root.joinpath(f'{fname}.xdf')
+        fname = self._data_root.joinpath(f"{fname}.xdf")
         if fname.exists() and not overwrite:
             # find latest suffix and add next
             fname = fname.parent.joinpath(
-                fname.stem + '_' + time.strftime('%Y%m%d%H%M%S') + fname.suffix)
+                fname.stem + "_" + time.strftime("%Y%m%d%H%M%S") + fname.suffix
+            )
 
         msg = f"filename {{root:{fname.parent}}} {{template:{fname.stem + fname.suffix}}}\n"
-        self._socket.sendall(msg.encode('UTF-8'))
+        self._socket.sendall(msg.encode("UTF-8"))
         time.sleep(1)
         return 0
 
@@ -79,10 +82,11 @@ class LSLRecorderCom(object):
         time.sleep(5)
         return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     communicator = LSLRecorderCom()
 
-    communicator.set_recording_file('testfile', data_root=Path('D:/'))
+    communicator.set_recording_file("testfile", data_root=Path("D:/"))
 
     communicator.record()
     time.sleep(3)
